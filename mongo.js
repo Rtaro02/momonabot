4048453c20b9f34efd885cde69899bcf36067e16
Ryotaro Takeda <takedarut@Ryotaros-MacBook-Air.local>
2020-06-29 23:28:22 +0900

    As a Bot, i wanna tweet completely so that i can deliver update of momona blog information to Momona fans.

---
+++ mongo.js
@@ -0,0 +1,22 @@
+const URI = "mongodb://root:example@mongohost:27017"; 
+const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const DB = 'fetch_result';
+const COLLECTION = 'ameba';
+
+exports.addAmebaResult = async function(obj) {
+    var client = await MongoClient.connect(URI, OPTIONS);
+    const db = client.db(DB);
+    var result = await db.collection(COLLECTION).insertOne(obj);
+    client.close();
+    return result.insertedCount;
+}
+
+exports.findAmebaResult = async function(obj) {
+    var client = await MongoClient.connect(URI, OPTIONS);
+    const db = client.db(DB);
+    var result = await db.collection(COLLECTION).findOne(obj);
+    client.close();
+    return result;
+}
