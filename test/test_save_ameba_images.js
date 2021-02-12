587d2d58ed13cbdb4041ddb10d6faa83b526d6d0
rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2021-02-13 08:26:32 +0900

    Fix ameba

--- test/test_save_ameba_images.js
+++ test/test_save_ameba_images.js
@@ -8,15 +8,15 @@ describe('アメブロ画像保存', function () {
     var actual;
     it('2020-07-12の莉佳子ちゃんのURLをインプットすると画像は三枚', async function () {
         actual = (await fetch_ameba.save('https://ameblo.jp/angerme-ss-shin/entry-12610610350.html?frm=theme'));
-        assert.equal(actual.length, 3);
+        assert.strictEqual(actual.names.length, 3);
     });
     it('2020-07-12の莉佳子ちゃんのURLをインプットすると画像名が返却される', async function () {
-        assert.equal(actual[0], 'o0958127814788062073.jpg');
-        assert.equal(actual[1], 'o1080144014788062087.jpg');
-        assert.equal(actual[2], 'o0958127814788062103.jpg');
+        assert.strictEqual(actual.names[0], 'o0958127814788062073.jpg');
+        assert.strictEqual(actual.names[1], 'o1080144014788062087.jpg');
+        assert.strictEqual(actual.names[2], 'o0958127814788062103.jpg');
     });
     it('2020-07-13の桃奈ちゃんはaタグが画像の前にあるけど画像が取れる', async function () {
         actual = (await fetch_ameba.save('https://ameblo.jp/angerme-ss-shin/entry-12610818468.html?frm=theme'));
-        assert.equal(actual[0], 'o1080080914788530830.jpg');
+        assert.strictEqual(actual.names[0], 'o1080080914788530830.jpg');
     });
 });
