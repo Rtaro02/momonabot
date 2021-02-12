7e9905a8e3ebf30cf5facfc64799d6f99b06dccf
rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2021-02-13 08:31:22 +0900

    Update eqaul sentecne

--- test/test_fetch_other_members.js
+++ test/test_fetch_other_members.js
@@ -8,26 +8,26 @@ describe('他のメンバーの桃奈ちゃん言及チェック', function () {
     it('20200624のふなっきのブログには笠原が入っている', async function () {
         var url = 'https://ameblo.jp/angerme-ss-shin/entry-12606619037.html';
         var actual = await fetch_ameba.check_momona_existence(url);
-        assert.equal(actual, true);
+        assert.strictEqual(actual, true);
     });
     it('20200625のささっきのブログはかっさーにふれていない', async function () {
         var url = 'https://ameblo.jp/angerme-ss-shin/entry-12606828457.html';
         var actual = await fetch_ameba.check_momona_existence(url);
-        assert.equal(actual, false);
+        assert.strictEqual(actual, false);
     });
     it('20170616のたけブログではももなっていっている', async function () {
         var url = 'https://ameblo.jp/angerme-amerika/entry-12284254745.html';
         var actual = await fetch_ameba.check_momona_existence(url);
-        assert.equal(actual, true);
+        assert.strictEqual(actual, true);
     });
     it('20190121のいせブログでは笠原っていっている', async function () {
         var url = 'https://ameblo.jp/angerme-new/entry-12434593254.html';
         var actual = await fetch_ameba.check_momona_existence(url);
-        assert.equal(actual, true);
+        assert.strictEqual(actual, true);
     });
     it('2020713の高瀬ブログでは笠原っていっている', async function () {
         var url = 'https://ameblo.jp/beyooooonds-rfro/entry-12610807938.html?frm=theme';
         var actual = await fetch_ameba.check_momona_existence(url);
-        assert.equal(actual, true);
+        assert.strictEqual(actual, true);
     });
 });
